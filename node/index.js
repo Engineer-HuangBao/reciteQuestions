9883e1aeda47d552d3689e7a1c4d3ebd26ad01a2
CVPD-Li <2687255946@qq.com>
2022-05-27 14:17:36 +0800

    请求dem、列表接口返回参数修改

--- node/index.js
+++ node/index.js
@@ -5,24 +5,14 @@ const querystring = require('querystring')
 let port = 3022 // 服务器端口号
 
 let fromData = {
-  html: {},
-  java: {},
-  css3: {},
-  javaScript: {},
-  webpack: {},
-  vite: {},
-  wechatApplet: {},
-  nodeJs: {},
-  app: {},
-  mySQL: {},
-  'c++': {},
-  php: {},
+  html: {}, java: {}, css3: {}, javaScript: {},
+  webpack: {}, vite: {}, wechatApplet: {}, nodeJs: {},
+  app: {}, mySQL: {}, 'c++': {}, php: {},
 }
 
 function initializationData () {
   Object.keys(fromData).map(item => {
     fs.readFile('./node/dataBase/' + item + '.json','utf8', function(err, datastr){
-      console.log(Object.prototype.toString.call(JSON.parse(datastr)['data']))
       let init = {
         "title": datastr ? JSON.parse(datastr)['title'] : "备注注解",
         "name": item.slice(0,1).toUpperCase() +item.slice(1).toLowerCase(),
@@ -46,32 +36,16 @@ server.on('request', (req, res) => {
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
   res.setHeader('Access-Control-Allow-Headers', ['mytoken', 'token','Content-Type']);
   res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
-  let datastr = null
-  let url = req.url
-  console.log(req.url)
 
   if (req.url === '/getList') return getList(res)
   if (req.url === '/add') res.end(`{a: 123}`)
   if (req.url === '/set') res.end(`{a: 123}`)
-  
-  // fs.readFile('./node/ceshi.json','utf8', function(err,datastr){
-  //   if (err) return  console.log('文件读取失败');
-  //   //  5.调用fs.writeFile()方法,把处理完毕后的成绩
-  //   // fs.writeFile('./node/ceshi.json', JSON.stringify({'宝': "娃"}),function(err){
-  //   //   // 读取失败 err的值也是为空  null转换为布尔值还是false
-  //   //   if (err) {
-  //   //     return console.log(err+'写入失败的');
-  //   //   }
-  //   //   console.log('成绩写入成功');
-  //   // })
-  //   console.log(datastr)
-  //   res.end(`${JSON.stringify(JSON.parse(datastr))}`)
-  // })
 })
 
 function getList(res) {
-  res.end(`{a: 222222222}`)
-  console.log(fromData)
+  let data = []
+  Object.keys(fromData).map(item => data.push(fromData[item]))
+  res.end(JSON.stringify(data))
 }
 
 
--- src/pages/answer.vue
+++ src/pages/answer.vue
@@ -1,22 +1,13 @@
 <script setup  lang="ts">
   import { ref, getCurrentInstance, onBeforeMount, onMounted } from 'vue'
   const { $https } = getCurrentInstance().appContext.config.globalProperties
+
+  const data = ref([])
   const aa = () => {
-    $https('getList', {cheshi: 123})
+    $https('getList', {cheshi: 123}).then(res => {
+      console.log(res)
+    })
   }
-  let data = [
-    { name: 'Html5', key: 'Html5' , title: '解释解释解释解释解', on: true },
-    { name: 'Css3', key: 'Css3' , title: '解释解释解释解释解', on: true },
-    { name: 'App', key: 'App' , title: '解释解释解释解释解', on: true },
-    { name: 'JavaScript', key: 'JavaScript' , title: '解释解释解释解释解', on: false },
-    { name: 'Webpack', key: 'Webpack' , title: '解释解释解释解释解', on: false },
-    { name: 'Vite', key: 'Vite' , title: '解释解释解释解释解', on: false },
-    { name: 'WechatApplet', key: 'WechatApplet' , title: '解释解释解释解释解', on: false },
-    { name: 'NodeJs', key: 'NodeJs' , title: '解释解释解释解释解', on: true },
-    { name: 'MySQL', key: 'MySQL' , title: '解释解释解释解释解', on: false },
-    { name: 'Java', key: 'Java' , title: '解释解释解释解释解', on: false },
-    { name: 'C++', key: 'C++' , title: '解释解释解释解释解', on: false },
-  ]
 </script>
 
 <template>
